bcb59fcee75590a3e825978fb63cd1efe8e5943c
David Smith <davidosmith@gmail.com>
2018-11-30 23:55:48 -0500

    Setup to send serial commands to circuit playground express

--- src/main.cpp
+++ src/main.cpp
@@ -12,22 +12,17 @@ Adafruit_MQTT_Subscribe tvcontrol = Adafruit_MQTT_Subscribe(&mqtt, MQTT_NAME "/f
 void MQTT_connect() {
   int8_t ret;
   // Stop if already connected
-  if (mqtt.connected())
-  {
-    return;
-  }
+  if (mqtt.connected()) return;
 
   Serial.print("Connecting to MQTT... ");
   uint8_t retries = 3;
-  while ((ret = mqtt.connect()) != 0) // connect will return 0 for connected
-  { 
+  while ((ret = mqtt.connect()) != 0) {   // connect will return 0 for connected
     Serial.println(mqtt.connectErrorString(ret));
     Serial.println("Retrying MQTT connection in 5 seconds...");
     mqtt.disconnect();
     delay(5000);  // wait 5 seconds
     retries--;
-    if (retries == 0) 
-    {
+    if (retries == 0){
       // basically die and wait for WDT to reset me
       while (1);
     }
@@ -58,24 +53,23 @@ void loop(){
   //Read from our subscription queue until we run out, or
   //wait up to 5 seconds for subscription to update
   Adafruit_MQTT_Subscribe * subscription;
-  while ((subscription = mqtt.readSubscription(5000)))
-  {
+  while ((subscription = mqtt.readSubscription(5000))){
     //If we're in here, a subscription updated...
-    if (subscription == &tvcontrol)
-    {
+    if (subscription == &tvcontrol){
+      char str[50];
+      sprintf(str, "esp%c", tvcontrol.lastread);    // send command to circuit playground express
+      Serial.print(str);                        
+
       //Print the new value to the serial monitor
       Serial.print("tvcontrol: ");
       Serial.println((char*) tvcontrol.lastread);
-
       //If the new value is  "ON", turn the light on.
       //Otherwise, turn it off.
-      if (!strcmp((char*) tvcontrol.lastread, "on"))
-      {
+      if (!strcmp((char*) tvcontrol.lastread, "on")){
         //active low logic
         digitalWrite(LED_BUILTIN, LOW);
       }
-      else if (!strcmp((char*) tvcontrol.lastread, "on"))
-      {
+      else if (!strcmp((char*) tvcontrol.lastread, "on")){
         digitalWrite(LED_BUILTIN, HIGH);
       }
     }
